55c712d4c3f7e2de7ecc4cc4c2e3fdfb660506d9
Mark Korzin <mark.korzin@gmail.com>
2025-11-07 18:44:11 +0200

    Created methods to work with elements. First part

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 import $ from './lib/lib';
 
 $('button').click(function() {
-    $(this).toggleClass('active');
+    $('div').eq(2).toggleClass('active');
 });
+
+$('div').click(function() {
+    console.log($(this).index());
+});
+
+console.log($('div').eq(2).find('.more'));
